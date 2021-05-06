e40a66171b5984a4e5fcd915fa657d4d7fed29a7
pakiZBRG <pavlovicnikola511@gmail.com>
2021-05-06 17:37:13 +0200

    Stylize Todays Highlights segment

--- components/FiveDayWeather.js
+++ components/FiveDayWeather.js
@@ -21,24 +21,43 @@ function FiveDayWeather({data}) {
                             </div>
                         ))}
                     </div>
-                    <h1>Today's Highlights</h1>
-                    <div className='flex flex-wrap justify-between'>
-                        <div className='flex flex-col bg-gray-700 p-10 text-2xl w-96 m-3'>
-                            <p>Wind Status</p>
-                            <p>{ weather[0].wind_speed.toFixed(0 )} mph</p>
-                            <p><small>{ weather[0].wind_direction.toFixed(0) }</small>{ weather[0].wind_direction_compass }</p>
+                    <h1 className='text-2xl font-bold pb-10'>Today's Highlights</h1>
+                    <div className='flex flex-wrap md:justify-center'>
+                        <div className='flex flex-col text-center bg-gray-700 px-10 text-2xl w-96 m-3 h-48'>
+                            <p className='py-4 text-base'>Wind Status</p>
+                            <p>
+                                <span className='font-black text-6xl'>{ weather[0].wind_speed.toFixed(0 )}</span> mph
+                            </p>
+                            <p className='pt-5'>
+                                <span className='bg-gray-500 px-3 pb-1 rounded-full m-3'>
+                                    <i style={{transform: `rotate(${weather[0].wind_direction.toFixed(0)}deg)`}} className="fa fa-location-arrow text-sm"></i>
+                                </span>
+                                <span className='text-sm'>{ weather[0].wind_direction_compass }</span>
+                            </p>
                         </div>
-                        <div className='flex flex-col bg-gray-700 p-10 text-2xl w-96 m-3'>
-                            <p>Humidity</p>
-                            <p>{ weather[0].humidity }%</p>
+                        <div className='flex flex-col text-center bg-gray-700 px-10 text-2xl w-96 m-3 h-48'>
+                            <p className='py-4 text-base'>Humidity</p>
+                            <p>
+                                <span className='font-black text-6xl'>{ weather[0].humidity }</span>%
+                            </p>
+                            <div className='bg-gray-100 relative mt-10 h-2 rounded-3xl'>
+                                <small className='absolute left-0 -top-5 text-xs'>0</small>
+                                <small className='absolute left-50 -top-5 text-xs'>50</small>
+                                <small className='absolute right-0 -top-5 text-xs'>100</small>
+                                <div style={{width: `${weather[0].humidity}%`}} className='absolute bg-yellow-300 h-2 rounded-l-3xl'></div>
                             </div>
-                        <div className='flex flex-col bg-gray-700 p-10 text-2xl w-96 m-3'>
-                            <p>Visibility</p>
-                            <p>{ weather[0].visibility.toFixed(1) } miles</p>
                         </div>
-                        <div className='flex flex-col bg-gray-700 p-10 text-2xl w-96 m-3'>
-                            <p>Air pressure</p>
-                            <p>{ weather[0].air_pressure } mbar</p>
+                        <div className='flex flex-col text-center bg-gray-700 px-10 text-2xl w-96 m-3 h-36'>
+                            <p className='py-4 text-base'>Visibility</p>
+                            <p className='pb-5'>
+                                <span className='font-black text-6xl'>{ weather[0].visibility.toFixed(1) }</span>miles
+                            </p>
+                        </div>
+                        <div className='flex flex-col text-center bg-gray-700 px-10 text-2xl w-96 m-3 h-36'>
+                            <p className='py-4 text-base'>Air pressure</p>
+                            <p className='pb-5'>
+                                <span className='font-black text-6xl'>{ weather[0].air_pressure }</span>mbar
+                            </p>
                         </div>
                     </div>
                 </div>
