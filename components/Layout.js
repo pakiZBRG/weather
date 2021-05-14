9456eee029509e01ce4f347d817c850e168bd93d
pakiZBRG <pavlovicnikola511@gmail.com>
2021-05-14 17:06:48 +0200

    Add cities in sidebar menu

--- components/Layout.js
+++ components/Layout.js
@@ -27,7 +27,12 @@ function Layout() {
     
     const handleChange = () => e => setCity(e.target.value);
 
-    const toggleSidebar = () => setOpen(!open)
+    const toggleSidebar = () => setOpen(!open);
+
+    const toggleCity = city => {
+        setCity(city);
+        setOpen(false);
+    }
 
     const weather = forecast && forecast.consolidated_weather;
 
@@ -58,6 +63,17 @@ function Layout() {
                                 />
                                 <span className='cursor-pointer text-4xl' onClick={toggleSidebar}>&times;</span>
                             </div>
+                                <div className='flex flex-col w-9/12'>
+                                    <span className='cursor-pointer text-xl p-2 m-2 border-2 border-gray-500' onClick={() => toggleCity('Madrid')}>
+                                        Madrid
+                                    </span>
+                                    <span className='cursor-pointer text-xl p-2 m-2 border-2 border-gray-500' onClick={() => toggleCity('Berlin')}>
+                                        Berlin
+                                    </span>
+                                    <span className='cursor-pointer text-xl p-2 m-2 border-2 border-gray-500' onClick={() => toggleCity('Mumbai')}>
+                                        Mumbai
+                                    </span>
+                                </div>
                         </div>
                     }
 
